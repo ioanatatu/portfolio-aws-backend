4e32fbc7dc5d992482792c323114014739f1b040
ioanatatu <tatu.ioana@gmail.com>
2020-12-17 22:50:37 +0100

    test

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("<h1>test</h1>");
 });
+app.get("/hola", (req, res) => {
+    res.json({ name: "pajarito", feathers: "colores de españa" });
+});
 
 const PORT = process.env.PORT || 4006;
 app.listen(PORT, () => {
