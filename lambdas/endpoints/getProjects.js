0827fc0eb77d3737927d0ea0e06d7b513b28074b
ioanatatu <tatu.ioana@gmail.com>
2020-12-23 17:30:54 +0100

    connect getProjects lambda to DynamoDB

--- lambdas/endpoints/getProjects.js
+++ lambdas/endpoints/getProjects.js
@@ -1,28 +1,26 @@
-const Responses = require("../lib/API_Responses");
+import AWS from "aws-sdk";
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = async (event) => {
-    return data
-        ? Responses._200(data)
-        : Responses._400({ message: "no ID in data" });
-};
+async function getProjects(event, context) {
+    let projects;
 
-const data = [
-    {
-        name: "thenap",
-        description:
-            "This is a sandbox project where I test ideas and implement features which I might want to use in future projects. It uses JWT authentication, manages state with Redux and stores data in two databases: PostgreSQL for users and Firebase for the subscribers emails.",
-        techStack: ["react", "redux"],
-        externalLink: "https://www.thenap.de/",
-        githubLink: "https://github.com/ioanatatu/take-a-nap",
-        logo: "FALSE",
-    },
-    {
-        name: "sinkplant",
-        description:
-            "Sinkplant is a task management app dedicated to scheduling tasks in a shared household. It has been developed by a former Spiced colleague as the final project at the end of the bootcamp. I found the idea inspiring and very approachable, so I decided to start contributing. The collaboration is also helping to me get more familiar with the github workflow.",
-        techStack: ["react", "redux"],
-        externalLink: "FALSE",
-        githubLink: "https://github.com/iclogg/sinkplant",
-        logo: "FALSE",
-    },
-];
+    const params = {
+        TableName: process.env.PROJECTS_TABLE_NAME,
+    };
+
+    try {
+        const result = await dynamodb.scan(params).promise();
+
+        projects = result.Items;
+    } catch (error) {
+        console.error(error);
+        throw Error("Error while fetching data");
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(projects),
+    };
+}
+
+export const handler = getProjects;
