f2d63ce24a6ba0cd1962eeed58f4b51cb6f83b2f
ioanatatu <tatu.ioana@gmail.com>
2021-01-05 23:40:28 +0100

    fixes in deleteProject and getJournalEntry

--- lambdas/endpoints/getJournalEntry.js
+++ lambdas/endpoints/getJournalEntry.js
@@ -7,21 +7,39 @@ const Responses = require("../lib/API_Responses");
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getJournalEntry(event) {
-    const journalEntryDate = event.pathParameters.journalEntryDate;
+    const pathParam = event.pathParameters;
+    console.log("pathParam", pathParam);
 
     let journalEntry;
-
     const queryKey = "#d";
+    let params = {};
 
-    const params = {
-        TableName: process.env.JOURNAL_TABLE_NAME,
-        ExpressionAttributeNames: { "#d": "date" },
-        KeyConditionExpression: `${queryKey} = :skey`,
-        ExpressionAttributeValues: {
-            ":skey": journalEntryDate,
-        },
-    };
+    if (!pathParam) {
+        // TODO: find a solution for querying the last entry from the db
+        const today = new Date().toISOString().split("T")[0];
 
+        params = {
+            TableName: process.env.JOURNAL_TABLE_NAME,
+            IndexName: "date-index",
+            ExpressionAttributeNames: { "#d": "date" },
+            KeyConditionExpression: `${queryKey} <= :hkey`,
+            ExpressionAttributeValues: {
+                ":hkey": today,
+            },
+            ScanIndexForward: "false",
+            Limit: 1,
+        };
+    } else {
+        params = {
+            TableName: process.env.JOURNAL_TABLE_NAME,
+            IndexName: "date-index",
+            ExpressionAttributeNames: { "#d": "date" },
+            KeyConditionExpression: `${queryKey} = :hkey`,
+            ExpressionAttributeValues: {
+                ":hkey": pathParam.journalEntryDate,
+            },
+        };
+    }
     try {
         const result = await dynamodb.query(params).promise();
 
